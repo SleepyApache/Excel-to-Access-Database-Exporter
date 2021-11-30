8ce27d8c6fecee9920e89811874df5150ff03577
Stefan Meeuwessen <55401579+SleepyApache@users.noreply.github.com>
2021-11-30 21:26:17 +0100

    Add files via upload

---
+++ Data Converter/Datatestje/Datatestje/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Datatestje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnrun = new System.Windows.Forms.Button();
+            this.btnbrowse = new System.Windows.Forms.Button();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnbrowse2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnrun
+            // 
+            this.btnrun.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(7)))), ((int)(((byte)(17)))));
+            this.btnrun.FlatAppearance.BorderSize = 0;
+            this.btnrun.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnrun.Font = new System.Drawing.Font("Lucida Fax", 8F);
+            this.btnrun.ForeColor = System.Drawing.Color.LightGray;
+            this.btnrun.Location = new System.Drawing.Point(15, 426);
+            this.btnrun.Name = "btnrun";
+            this.btnrun.Size = new System.Drawing.Size(237, 45);
+            this.btnrun.TabIndex = 0;
+            this.btnrun.Text = "Run";
+            this.btnrun.UseVisualStyleBackColor = false;
+            this.btnrun.Click += new System.EventHandler(this.btnrun_Click);
+            // 
+            // btnbrowse
+            // 
+            this.btnbrowse.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(7)))), ((int)(((byte)(17)))));
+            this.btnbrowse.FlatAppearance.BorderSize = 0;
+            this.btnbrowse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnbrowse.Font = new System.Drawing.Font("Lucida Fax", 8F);
+            this.btnbrowse.ForeColor = System.Drawing.Color.LightGray;
+            this.btnbrowse.Location = new System.Drawing.Point(15, 168);
+            this.btnbrowse.Name = "btnbrowse";
+            this.btnbrowse.Size = new System.Drawing.Size(237, 45);
+            this.btnbrowse.TabIndex = 1;
+            this.btnbrowse.Text = "Browse";
+            this.btnbrowse.UseVisualStyleBackColor = false;
+            this.btnbrowse.Click += new System.EventHandler(this.btnbrowse_Click_1);
+            // 
+            // btnsave
+            // 
+            this.btnsave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnsave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(7)))), ((int)(((byte)(17)))));
+            this.btnsave.FlatAppearance.BorderSize = 0;
+            this.btnsave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnsave.Font = new System.Drawing.Font("Lucida Fax", 8F);
+            this.btnsave.ForeColor = System.Drawing.Color.LightGray;
+            this.btnsave.Location = new System.Drawing.Point(15, 684);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(237, 45);
+            this.btnsave.TabIndex = 2;
+            this.btnsave.Text = "Save to Database";
+            this.btnsave.UseVisualStyleBackColor = false;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(7)))), ((int)(((byte)(17)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Lucida Fax", 8F);
+            this.textBox1.ForeColor = System.Drawing.Color.LightGray;
+            this.textBox1.Location = new System.Drawing.Point(15, 141);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(237, 16);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Fax", 8F);
+            this.label1.ForeColor = System.Drawing.Color.LightGray;
+            this.label1.Location = new System.Drawing.Point(12, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Excel File Path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Fax", 8F);
+            this.label2.ForeColor = System.Drawing.Color.LightGray;
+            this.label2.Location = new System.Drawing.Point(12, 380);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Type Excel Sheet Name";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(32)))), ((int)(((byte)(39)))));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(319, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(931, 717);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(7)))), ((int)(((byte)(17)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Lucida Fax", 8F);
+            this.textBox2.ForeColor = System.Drawing.Color.LightGray;
+            this.textBox2.Location = new System.Drawing.Point(15, 272);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(237, 16);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Fax", 8F);
+            this.label3.ForeColor = System.Drawing.Color.LightGray;
+            this.label3.Location = new System.Drawing.Point(12, 252);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Database File Path";
+            // 
+            // btnbrowse2
+            // 
+            this.btnbrowse2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(7)))), ((int)(((byte)(17)))));
+            this.btnbrowse2.FlatAppearance.BorderSize = 0;
+            this.btnbrowse2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnbrowse2.Font = new System.Drawing.Font("Lucida Fax", 8F);
+            this.btnbrowse2.ForeColor = System.Drawing.Color.LightGray;
+            this.btnbrowse2.Location = new System.Drawing.Point(15, 299);
+            this.btnbrowse2.Name = "btnbrowse2";
+            this.btnbrowse2.Size = new System.Drawing.Size(237, 45);
+            this.btnbrowse2.TabIndex = 10;
+            this.btnbrowse2.Text = "Browse";
+            this.btnbrowse2.UseVisualStyleBackColor = false;
+            this.btnbrowse2.Click += new System.EventHandler(this.btnbrowse2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(32)))), ((int)(((byte)(39)))));
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.btnbrowse2);
+            this.panel1.Controls.Add(this.btnsave);
+            this.panel1.Controls.Add(this.btnrun);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnbrowse);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(277, 741);
+            this.panel1.TabIndex = 11;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Monotype Corsiva", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Moccasin;
+            this.label4.Location = new System.Drawing.Point(22, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(213, 40);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Data Converter";
+            // 
+            // textBox3
+            // 
+            this.textBox3.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.textBox3.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(7)))), ((int)(((byte)(17)))));
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Lucida Fax", 8F);
+            this.textBox3.ForeColor = System.Drawing.Color.LightGray;
+            this.textBox3.Location = new System.Drawing.Point(15, 399);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(237, 16);
+            this.textBox3.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(7)))), ((int)(((byte)(17)))));
+            this.ClientSize = new System.Drawing.Size(1262, 741);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1600, 900);
+            this.MinimumSize = new System.Drawing.Size(1280, 788);
+            this.Name = "Form1";
+            this.Text = "Data Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnrun;
+        private System.Windows.Forms.Button btnbrowse;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnbrowse2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Data Converter/Datatestje/Datatestje/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Datatestje
+{
+    #region Form
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+
+        //You can only use this program with .xls and .mdb files. Further to use it your self you will need to change some values further in the code.
+        // This has all been written out by me further down the code.
+        #region Buttons
+        private void btnrun_Click(object sender, EventArgs e)
+        {
+            string EXpath = textBox1.Text;
+            string PathConn = "Provider = Microsoft.Jet.OLEDB.4.0; Data Source =" + EXpath + ";Extended Properties=\"Excel 8.0;HDR=Yes;\";";
+            OleDbConnection conn = new OleDbConnection(PathConn);
+
+            try
+            {
+                string EXsheet = textBox3.Text;
+                var sqlQuery = "Select * from [" + EXsheet + "$]";
+                OleDbDataAdapter myDataAdapter = new OleDbDataAdapter(sqlQuery, conn);
+                DataTable dt = new DataTable();
+                myDataAdapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            catch
+            { 
+                
+            }
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            {
+                string EXpath = textBox1.Text;
+                string fileNameExcel = @EXpath;
+                string ACpath = textBox2.Text;
+                string fileNameAccess = @ACpath;
+                string EXsheet = textBox3.Text;
+
+                string connectionStringExcel =
+                    string.Format("Data Source= {0};Provider=Microsoft.Jet.OLEDB.4.0;Extended Properties=Excel 8.0;", fileNameExcel);
+
+                string ConnectionStringAccess =
+                    string.Format("Data Source= {0};Provider=Microsoft.Jet.OLEDB.4.0; Persist security Info = false", fileNameAccess);
+
+                OleDbConnection connExcel = new OleDbConnection(connectionStringExcel);
+                OleDbConnection connAccess = new OleDbConnection(ConnectionStringAccess);
+                OleDbCommand cmdExcel = connExcel.CreateCommand();
+                cmdExcel.CommandType = CommandType.Text;
+                cmdExcel.CommandText = "SELECT * from [" + EXsheet + "$]";
+                OleDbCommand cmdAccess = connAccess.CreateCommand();
+                cmdAccess.CommandType = CommandType.Text;
+                
+
+                //Add parameters *
+                //Here you need to change the names of the columns.
+                //These should be called the same in Excel and Access.
+                //Also change - Informatie - To the name of your Access Database Table.
+                cmdAccess.CommandText = "INSERT INTO Informatie (Naam, Achternaam, Land, Stad, Huisnummer, Postcode, Telefoonnummer) VALUES(@Naam, @Achternaam, @Land, @Stad, @Huisnummer, @Postcode, @Telefoonnummer)";
+
+
+                //Add parameters to Access command object **
+                //All you need to do here is change the names of the columns.
+                //These should be called the same in Excel and Access.
+                OleDbParameter param1 = new OleDbParameter("@Naam", OleDbType.VarChar);
+                cmdAccess.Parameters.Add(param1);
+                OleDbParameter param2 = new OleDbParameter("@Achternaam", OleDbType.VarChar);
+                cmdAccess.Parameters.Add(param2);
+                OleDbParameter param3 = new OleDbParameter("@Land", OleDbType.VarChar);
+                cmdAccess.Parameters.Add(param3);
+                OleDbParameter param4 = new OleDbParameter("@Stad", OleDbType.VarChar);
+                cmdAccess.Parameters.Add(param4);
+                OleDbParameter param5 = new OleDbParameter("@Huisnummer", OleDbType.VarChar);
+                cmdAccess.Parameters.Add(param5);
+                OleDbParameter param6 = new OleDbParameter("@Postcode", OleDbType.VarChar);
+                cmdAccess.Parameters.Add(param6);
+                OleDbParameter param7 = new OleDbParameter("@Telefoonnummer", OleDbType.VarChar);
+                cmdAccess.Parameters.Add(param7);
+
+                connExcel.Open();
+                connAccess.Open();
+                OleDbDataReader drExcel = cmdExcel.ExecuteReader();
+
+                while (drExcel.Read())
+                {
+
+                    //Assign values to access command parameters ***
+                    // To add more columns you just copy what it has and add more. F.E - param8.Value = drExcel[7].ToString();
+                    param1.Value = drExcel[0].ToString();
+                    param2.Value = drExcel[1].ToString();
+                    param3.Value = drExcel[2].ToString();
+                    param4.Value = drExcel[3].ToString();
+                    param5.Value = drExcel[4].ToString();
+                    param6.Value = drExcel[5].ToString();
+                    param7.Value = drExcel[6].ToString();
+
+                    cmdAccess.ExecuteNonQuery();
+                }
+
+                connAccess.Close();
+                connExcel.Close();
+                MessageBox.Show("Succesfully uploaded Excel data to Database.");
+
+            }
+        }
+
+        private void btnbrowse_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog openfiledialog1 = new OpenFileDialog();
+            openfiledialog1.ShowDialog();
+            openfiledialog1.Filter = "allfiles|*.xls";
+            textBox1.Text = openfiledialog1.FileName;
+        }
+
+        private void btnbrowse2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openfiledialog1 = new OpenFileDialog();
+            openfiledialog1.ShowDialog();
+            openfiledialog1.Filter = "allfiles|*.mdb";
+            textBox2.Text = openfiledialog1.FileName;
+        }
+        #endregion
+
+
+        #region Extra
+
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        #endregion
+    }
+}
